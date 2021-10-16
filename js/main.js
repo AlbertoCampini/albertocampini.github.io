d18947956c573cbf8670336ad565e2eb855b68f9
Alberto <campinialberto@gmail.com>
2021-10-16 17:27:59 +0200

    Load all file

---
+++ js/main.js
@@ -0,0 +1,263 @@
+
+
+
+function wrapperChangeLanguage() {
+    if (running_language == "it") {
+        changeLanguage(json_langauge.english, "en")
+
+    } else {
+
+        changeLanguage(json_langauge.italiano, "it")
+
+    }
+}
+function changeLanguage(language, string) {
+
+    Object.keys(language).forEach(function (key) {
+        $("#" + language[key].id).html(language[key].html)
+    });
+    running_language = string
+}
+
+function submitMessage() {
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const check = document.getElementById("check").checked;
+
+    const outputMessage = document.getElementById("outputMessage");
+
+    if(name !== '' && surname !== '' && email !== '' && message !== '' && check === true){
+
+    }else{
+        outputMessage.innerHTML= "Controlla di aver inserito correttamente i dati"
+    }
+
+}
+
+function changeColor(value){
+    document.documentElement.style.setProperty("--hue-color", value);
+
+}
+
+
+
+
+/*==================== MANAGE LANGUAGE ====================*/
+let running_language = (navigator.language || navigator.userLanguage).substring(0, 2);
+
+
+changeLanguage(json_langauge.english, "en")
+
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navMenuColor = document.getElementById('nav-menu-color'),
+    navToggle = document.getElementById('nav-toggle'),
+    navToggleColor = document.getElementById('nav-toggle-color'),
+    navCloseColor = document.getElementById('nav-close-color'),
+    navClose = document.getElementById('nav-close'),
+    navChangeLanguages = document.querySelectorAll('.change-language')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navToggleColor) {
+    navToggleColor.addEventListener('click', () => {
+        navMenuColor.classList.add('show-menu')
+        console.log("hello")
+    })
+}
+navChangeLanguages.forEach(navChangeLanguage =>{
+    if (navChangeLanguage) {
+        navChangeLanguage.addEventListener('click', () => {
+            wrapperChangeLanguage()
+        })
+    }
+})
+
+
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+        console.log("ciao13")
+    })
+}
+
+if (navCloseColor) {
+    navCloseColor.addEventListener('click', () => {
+        navMenuColor.classList.remove('show-menu')
+        console.log("ciao1")
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.ClassName
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+
+    this.parentNode.className = 'skills__content skills__open'
+
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+
+        target.classList.add('qualification__active')
+
+        tabs.forEach(t => {
+            t.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+
+
+
+})
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+  
+  });
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            if(document.querySelector('.nav__menu a[href*=' + sectionId + ']') !== null)
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            if(document.querySelector('.nav__menu a[href*=' + sectionId + ']') !== null)
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add(darkTheme)
+    themeButton.classList.add(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+}else{
+    document.body.classList.remove(darkTheme)
+    themeButton.classList.remove(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+}
+
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
