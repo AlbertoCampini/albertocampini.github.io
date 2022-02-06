a3e6a3814b5a10056c69090991d593988b128c86
Alberto <campinilaberto@gmail.com>
2022-02-06 18:06:31 +0100

    Versione da mandare online, minimizzati i css e aggiunti suoni

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /*==================== MANAGE TOASTY ====================*/
 let options = {
     classname: "toast",
@@ -14,12 +13,21 @@ let options = {
         warning: "sounds/warning/1.mp3",
         error: "sounds/error/1.mp3",
     },
-    onShow: function (type) {},
-    onHide: function (type) {},
+    onShow: function (type) {
+    },
+    onHide: function (type) {
+    },
     prependTo: document.body.childNodes[0]
 };
 
 let toast = new Toasty(options)
+
+let counterEsterEgg = 0;
+
+document.getElementById("footer-name").addEventListener("click", () => {
+    counterEsterEgg = counterEsterEgg + 1;
+    if (counterEsterEgg === 3) console.log("v")
+})
 
 /*==================== MANAGE LANGUAGE ====================*/
 let running_language = (navigator.language || navigator.userLanguage).substring(0, 2);
@@ -27,8 +35,11 @@ let running_language = (navigator.language || navigator.userLanguage).substring(
 changeLanguage(running_language)
 
 function wrapperChangeLanguage() {
-    if (running_language === 'it') {
+    const audioChangeLanguage = document.getElementById("audio-change-language")
 
+    options.enableSounds ? audioChangeLanguage.play() : audioChangeLanguage
+
+    if (running_language === 'it') {
         changeLanguage('en')
     } else {
 
@@ -36,6 +47,7 @@ function wrapperChangeLanguage() {
 
     }
 }
+
 function changeLanguage(string) {
     let language = string === 'en' ? json_langauge.english : json_langauge.italiano
     Object.keys(language).forEach(function (key) {
@@ -50,19 +62,33 @@ function submitMessage() {
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
     const check = document.getElementById("check").checked;
-    const outputMessage = document.getElementById("outputMessage");
 
 
+    if (name !== '' && surname !== '' && email !== '' && message !== '' && check === true && /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+
+        $.ajax({
+            method: "POST",
+            url: "",
+            data: {
+                name: name,
+                surname: surname,
+                email: email,
+                message: message
+            },
+            success: () => {
+                running_language === 'it' ? toast.success("Messaggio inviato correttamente") : toast.success("Message sent correctly")
+                document.querySelectorAll(".contact__input").forEach(function (element) {
+                    element.value = ""
+                });
+            },
+            error: () => {
+                running_language === 'it' ? toast.error("Controlla di aver inserito correttamente i dati all'interno del form") : toast.error("Check that you have correctly inserted the data within the module")
+            }
+        });
 
 
-    if(name !== '' && surname !== '' && email !== '' && message !== '' && check === true && /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
-        running_language === 'it' ?  toast.success("Messaggio inviato correttamente") :toast.success("Message sent correctly")
-        document.querySelectorAll(".contact__input").forEach(function (element){
-            element.value = ""
-        })
-
-    }else{
-        running_language === 'it' ? toast.error("Controlla di aver inserito correttamente i dati all'interno del form"):toast.error("Check that you have correctly inserted the data within the module")
+    } else {
+        running_language === 'it' ? toast.error("Controlla di aver inserito correttamente i dati all'interno del form") : toast.error("Check that you have correctly inserted the data within the module")
 
     }
 
@@ -77,7 +103,7 @@ const navMenu = document.getElementById('nav-menu'),
     navCloseColor = document.getElementById('nav-close-color'),
     navClose = document.getElementById('nav-close'),
     navChangeLanguages = document.querySelectorAll('.change-language')
-    navChangeVolume= document.querySelectorAll('.change-volume')
+navChangeVolume = document.querySelectorAll('.change-volume')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -92,7 +118,7 @@ if (navToggleColor) {
         navMenuColor.classList.add('show-menu')
     })
 }
-navChangeLanguages.forEach(navChangeLanguage =>{
+navChangeLanguages.forEach(navChangeLanguage => {
     if (navChangeLanguage) {
         navChangeLanguage.addEventListener('click', () => {
             wrapperChangeLanguage()
@@ -100,17 +126,20 @@ navChangeLanguages.forEach(navChangeLanguage =>{
     }
 })
 
-navChangeVolume.forEach(navChangeVolume =>{
+navChangeVolume.forEach(navChangeVolume => {
     const topBarVolume = document.getElementById('change-volume-topbar')
+    const audioChangeVolume = document.getElementById("audio-change-volume")
+
+
     if (navChangeVolume) {
         navChangeVolume.addEventListener('click', () => {
             options.enableSounds = !options.enableSounds
             toast = new Toasty(options)
-
-            if(topBarVolume.classList.contains("uil-volume")){
+            options.enableSounds ? audioChangeVolume.play() : audioChangeVolume
+            if (topBarVolume.classList.contains("uil-volume")) {
                 topBarVolume.classList.remove("uil-volume")
                 topBarVolume.classList.add("uil-volume-mute")
-            }else{
+            } else {
                 topBarVolume.classList.add("uil-volume")
                 topBarVolume.classList.remove("uil-volume-mute")
             }
@@ -118,7 +147,39 @@ navChangeVolume.forEach(navChangeVolume =>{
     }
 })
 
+/*=============== SHOW MODAL ===============*/
 
+
+function openModal(target) {
+    const modalContainer = document.getElementById('modal-container')
+    const modalContentTerm = document.getElementById('modal__content__Term')
+    const modalContentAbout = document.getElementById('modal__content__about')
+
+    switch (target) {
+        case 'About':
+            modalContentTerm.classList.add("d-none")
+            modalContentAbout.classList.remove("d-none")
+            break;
+        case 'Term':
+            modalContentTerm.classList.remove("d-none")
+            modalContentAbout.classList.add("d-none")
+            break;
+
+    }
+    modalContainer.classList.add('show-modal')
+
+
+}
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal() {
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
 
 
 /*===== MENU HIDDEN =====*/
@@ -143,6 +204,7 @@ function linkAction() {
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
@@ -186,7 +248,6 @@ tabs.forEach(tab => {
     })
 
 
-
 })
 
 
@@ -198,15 +259,15 @@ let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable:true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-  
-  });
+
+});
 
 
 /*==================== TESTIMONIAL ====================*/
@@ -215,40 +276,43 @@ let swiper = new Swiper(".portfolio__container", {
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            if(document.querySelector('.nav__menu a[href*=' + sectionId + ']') !== null)
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            if(document.querySelector('.nav__menu a[href*=' + sectionId + ']') !== null)
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            if (document.querySelector('.nav__menu a[href*=' + sectionId + ']') !== null)
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            if (document.querySelector('.nav__menu a[href*=' + sectionId + ']') !== null)
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
+
 window.addEventListener('scroll', scrollActive)
 
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
+function scrollHeader() {
     const nav = document.getElementById('header')
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
+
 window.addEventListener('scroll', scrollHeader)
 
 
 /*==================== SHOW SCROLL UP ====================*/
-function scrollUp(){
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
+
 window.addEventListener('scroll', scrollUp)
 
 
@@ -256,6 +320,8 @@ window.addEventListener('scroll', scrollUp)
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
+const audioSwitchOn = document.getElementById("audio-switch-on")
+const audioSwitchOff = document.getElementById("audio-switch-off")
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -267,9 +333,9 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -278,7 +344,7 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
-}else{
+} else {
     document.body.classList.remove(darkTheme)
     themeButton.classList.remove(iconTheme)
 
@@ -292,11 +358,37 @@ themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
+
+    //Manage the sound of switch
+    options.enableSounds ? getCurrentTheme() === 'light' ? audioSwitchOn.play() : audioSwitchOff.play() :
+
+        // We save the theme and the current icon that the user chose
+        localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-function changeColor(value){
+
+function changeColor(value) {
     document.documentElement.style.setProperty("--hue-color", value);
 
 }
+
+/*==================== SCROLL REVEAL ====================*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1500,
+    delay: 300,
+})
+
+sr.reveal('.home__img')
+sr.reveal('.skills__content', {interval: 100})
+sr.reveal('.about__info__content', {interval: 100})
+sr.reveal('.qualification__tabs', {delay: 200})
+sr.reveal('.qualification__sections')
+sr.reveal('.swiper-container')
+sr.reveal('.contact__information', {origin: 'left', interval: 100})
+sr.reveal('.contact__content', {origin: 'left', interval: 100})
+
+
+
